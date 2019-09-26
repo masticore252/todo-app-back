1c8b0fbfb3c6a7a8255941acdacee96b046c085a
Albert Vásquez <albert.vasquez@bairesdev.com>
2019-09-26 17:07:50 -0500

    add Task model, migration, controller and factory

---
+++ database/migrations/2019_09_26_212626_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description', 100);
+            $table->string('state', 10);
+            $table->string('attachment', 255);
+            $table->string('attachment_type', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
